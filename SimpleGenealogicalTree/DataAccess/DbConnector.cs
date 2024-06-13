d8235c1c574d6138f56f38aa0e201ebc7a61b8b7
vadimartyushenko <vadim.artyuschenko@yandex.ru>
2024-06-13 18:27:56 +0700

    Add project files.

---
+++ SimpleGenealogicalTree/DataAccess/DbConnector.cs
@@ -0,0 +1,19 @@
+﻿using RepoDb;
+using System.Data.SQLite;
+
+namespace SimpleGenealogicalTree.DataAccess;
+
+public class DbConnector
+{
+    string connectionString = new SQLiteConnectionStringBuilder() {
+        DataSource = "family_tree.db",
+        ForeignKeys = true,
+    }.ToString();
+
+    public DbConnector()
+    {
+        GlobalConfiguration.Setup().UseSQLite();
+        using var connection = new SQLiteConnection(connectionString);
+        DbSchema.Update(connection);
+    }
+}
---
+++ SimpleGenealogicalTree/DataAccess/DbSchema.cs
@@ -0,0 +1,32 @@
+﻿using RepoDb;
+using System.Data.SQLite;
+
+namespace SimpleGenealogicalTree.DataAccess;
+
+public class DbSchema
+{
+    const int VERSION = 1;
+
+    //record Setting(string Key, string Value);
+
+    public static void Update(SQLiteConnection connection)
+    {
+        var emptyDb = connection.ExecuteScalar("SELECT name FROM sqlite_master WHERE type='table' AND name='Setting'") == null;
+        if (emptyDb)
+        {
+            connection.ExecuteNonQuery(@"CREATE TABLE IF NOT EXISTS Setting (
+                    [Key] TEXT NOT NULL
+                                 PRIMARY KEY,
+                    Value TEXT NOT NULL
+                );");
+
+            connection.ExecuteNonQuery(@"CREATE TABLE IF NOT EXISTS Member (
+                    Id             INTEGER PRIMARY KEY AUTOINCREMENT
+                                           NOT NULL,
+                    Name           TEXT    NOT NULL,
+                    Surname        TEXT    NOT NULL,
+                    BirthDateTicks INTEGER NOT NULL,
+                );");
+        }
+    }
+}
